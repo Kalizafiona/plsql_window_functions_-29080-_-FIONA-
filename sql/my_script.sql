004926a5f1acd9baf1f681fccabd00f59591fe2e
Kalizafiona <kalizafiona95@gmail.com>
2026-02-08 15:06:58 +0200

    Add course information to my_script.sql

---
+++ sql/my_script.sql
@@ -0,0 +1,5 @@
+ECOQUEST ADVENTURES: SQL JOINs & Window Functions
+Course: INSY 8311 - Database Development with PL/SQL
+Student: KALIZA Fiona | 29080 | Group A
+Instructor: Eric Maniraguha
+Submission Date: February 08, 2025
